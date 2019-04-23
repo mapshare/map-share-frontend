2e22deef536b90b9ad13381f61cc4ab948ef9678
KcJam3 <kc.liang@jam3.com>
2019-04-23 09:57:44 -0400

    deployment to heroku

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+"start": "react-scripts start",
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ build/precache-manifest.a1641525db3e1d7562056003bee7d3fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c90108bee4e8f1f5744a",
+    "url": "/static/js/main.c90108be.chunk.js"
+  },
+  {
+    "revision": "71ed6ca9931ce1ea5c88",
+    "url": "/static/js/1.71ed6ca9.chunk.js"
+  },
+  {
+    "revision": "c90108bee4e8f1f5744a",
+    "url": "/static/css/main.2b88424d.chunk.css"
+  },
+  {
+    "revision": "71ed6ca9931ce1ea5c88",
+    "url": "/static/css/1.b9fcadde.chunk.css"
+  },
+  {
+    "revision": "c07eb69a4b0ec37ff0be9112488ed90a",
+    "url": "/index.html"
+  }
+];
