010f8f27eab90105c83507f0bc4f887138f6a4e5
kc-leung <chiu-leung@Hotmail.com>
2019-05-08 15:15:02 -0400

    Clean up SignInForm & GroupLanding Components

--- src/components/GroupLanding/GroupLanding.js
+++ src/components/GroupLanding/GroupLanding.js
@@ -1,62 +1,76 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
 
-//import dispatch actions for create/join group
 import "./GroupLanding.scss";
+
 import GroupForm from "../Forms/GroupForm/GroupForm";
 
+const GroupLanding = React.forwardRef((props, ref) => {
+  return (
+    <div className={classnames("GroupLanding", props.className)}>
+      <div className="container">
+        <div className="card text-center rounded">
+          <img className="card-img-top" src="" alt="" />
+          <div className="card-body">
+            <h4 className="card-title">You aren't in a group yet!</h4>
+            <p className="card-text">Please join or create a group</p>
 
-export class GroupLanding extends Component {
-  render() {
-    return (
-      <div className="container-fluid groupForm-container">
-        <div className="container">
-          <div className="card text-center rounded">
-            <img className="card-img-top" src="" alt="" />
-            <div className="card-body">
-              <h4 className="card-title">
-                You aren't in a group yet!
-              </h4>
-              <p className="card-text">Please join or create a group</p>
-
-              <div>
-                <p>
-                  <a className="btn btn-primary group-option collapsed" data-toggle="collapse" href="#joinGroup" role="button" aria-expanded="false" aria-controls="joinGroup">
-                    Join Group
-                  </a>
-                  <a className="btn btn-primary group-option collapsed" data-toggle="collapse" href="#createGroup" role="button" aria-expanded="false" aria-controls="createGroup">
-                    Create Group
-                  </a>
-                </p>
-                <div id="groupWrapper">
-                  <div className="collapse" id="createGroup" data-parent="#groupWrapper">
-                    <div className="card-body">
-                      <GroupForm text={"New Group Name"} action={"create"} />
-                    </div>
+            <div>
+              <p>
+                <a
+                  className="btn btn-primary group-option collapsed"
+                  data-toggle="collapse"
+                  href="#joinGroup"
+                  role="button"
+                  aria-expanded="false"
+                  aria-controls="joinGroup"
+                >
+                  Join Group
+                </a>
+                <a
+                  className="btn btn-primary group-option collapsed"
+                  data-toggle="collapse"
+                  href="#createGroup"
+                  role="button"
+                  aria-expanded="false"
+                  aria-controls="createGroup"
+                >
+                  Create Group
+                </a>
+              </p>
+              <div id="groupWrapper">
+                <div
+                  className="collapse"
+                  id="createGroup"
+                  data-parent="#groupWrapper"
+                >
+                  <div className="card-body">
+                    <GroupForm text={"New Group Name"} action={"create"} />
                   </div>
-                  <div className="collapse" id="joinGroup" data-parent="#groupWrapper">
-                    <div className="card-body">
-                      <GroupForm text={"Existing Group Id"} action={"join"} />
-                    </div>
+                </div>
+                <div
+                  className="collapse"
+                  id="joinGroup"
+                  data-parent="#groupWrapper"
+                >
+                  <div className="card-body">
+                    <GroupForm text={"Existing Group Id"} action={"join"} />
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+});
 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     signInSuccess: bool => dispatch(signInSuccess(bool))
-//   };
-// };
+GroupLanding.propTypes = {
+  className: PropTypes.string
+};
 
-export default connect(
-  null,
-  null
-)(GroupLanding);
+GroupLanding.defaultProps = {};
+
+export default GroupLanding;
--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
 import { connect } from "react-redux";
 import { GoogleLogin } from "react-google-login";
 
@@ -6,56 +8,19 @@ import "./SignInForm.scss";
 
 import { signInSuccess, postUser } from "../../actions/signInActions";
 
+import { GOOGLE_LOGIN_TOKEN } from "../../data/constants";
+
 export class SignInForm extends Component {
-  state = { errorMessage: null };
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessage: null
+    };
+  }
 
   handleSignIn = bool => {
     this.props.signInSuccess(bool);
   };
-
-  // componentDidMount() {
-  //   this.temp();
-  // }
-
-  // temp = response => {
-  //   console.log("calling temp response: ", response);
-  //   if (response) {
-  //     console.log("yes response!!");
-  //     return <MapContainer />;
-  //   } else {
-  //     return (
-  //       <div className="container-fluid signInForm-container">
-  //         <div className="container">
-  //           <div className="card text-center rounded">
-  //             <img className="card-img-top" src="" alt="" />
-  //             <div className="card-body">
-  //               <h1 className="card-title">
-  //                 Welcome to
-  //                 <br />
-  //                 map-share-api!
-  //               </h1>
-  //               <p className="card-text">This is the best webapp ever.</p>
-
-  //               <GoogleLogin
-  //                 className="btn"
-  //                 clientId="249486761636-7v9e2d4eopnh2hcedo5fa3uu0uqvg7t1.apps.googleusercontent.com"
-  //                 buttonText="Login with Google!"
-  //                 onSuccess={this.temp}
-  //                 onFailure={this.temp}
-  //                 isSignedIn={true}
-  //               />
-  //               {this.state.errorMessage ? (
-  //                 <div className="error-message">
-  //                   Please sign in to use map-share-api
-  //                 </div>
-  //               ) : null}
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     );
-  //   }
-  // };
 
   responseGoogle = response => {
     if (response.error) {
@@ -69,7 +34,7 @@ export class SignInForm extends Component {
 
   render() {
     return (
-      <div className="container-fluid signInForm-container">
+      <div className={classnames("SignInForm", this.props.className)}>
         <div className="container">
           <div className="card text-center rounded">
             <img className="card-img-top" src="" alt="" />
@@ -83,17 +48,17 @@ export class SignInForm extends Component {
 
               <GoogleLogin
                 className="btn"
-                clientId="249486761636-7v9e2d4eopnh2hcedo5fa3uu0uqvg7t1.apps.googleusercontent.com"
+                clientId={GOOGLE_LOGIN_TOKEN}
                 buttonText="Login with Google!"
                 onSuccess={this.responseGoogle}
                 onFailure={this.responseGoogle}
                 isSignedIn={true}
               />
-              {this.state.errorMessage ? (
+              {this.state.errorMessage && (
                 <div className="error-message">
                   Please sign in to use Map-Share
                 </div>
-              ) : null}
+              )}
             </div>
           </div>
         </div>
@@ -101,6 +66,14 @@ export class SignInForm extends Component {
     );
   }
 }
+
+SignInForm.propTypes = {
+  className: PropTypes.string,
+  signInSuccess: PropTypes.func.isRequired,
+  postUser: PropTypes.func.isRequired
+};
+
+SignInForm.defaultProps = {};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -113,21 +86,3 @@ export default connect(
   null,
   mapDispatchToProps
 )(SignInForm);
-
-/*<div className="container-fluid login-button">
-<div className="col-md-12 text-center">
-<button id="singlebutton" name="singlebutton" className="btn btn-primary">
-  <GoogleLogin
-    clientId="249486761636-7v9e2d4eopnh2hcedo5fa3uu0uqvg7t1.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={this.responseGoogle}
-    onFailure={this.responseGoogle}
-    isSignedIn={true}
-  />
-</button>
-</div>
-</div>*/
-
-// render={renderProps => (
-//   <button className="btn btn-primary" onClick={renderProps.onClick}>This is my custom Google button</button>
-// )}
